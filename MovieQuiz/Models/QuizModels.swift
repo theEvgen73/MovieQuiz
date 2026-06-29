4736181e28e0c9e6ffdad5ae2ac94e6f569c5d85
Evgenii <e.paprotsky@gmail.com>
2026-06-29 14:45:40 +0400

    дополнил

---
+++ MovieQuiz/Models/QuizModels.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
+
+struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
